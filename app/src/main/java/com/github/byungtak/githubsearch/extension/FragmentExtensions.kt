b620c8ca2630e5cfaf65264bea107c2eab8a3436
Tak <tak8997@gmail.com>
2019-04-22 23:32:12 +0900

    bottom navigation 선택 방식 변경

--- app/src/main/java/com/github/byungtak/githubsearch/extension/FragmentExtensions.kt
+++ app/src/main/java/com/github/byungtak/githubsearch/extension/FragmentExtensions.kt
@@ -1,12 +1,41 @@
 package com.github.byungtak.githubsearch.extension
 
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.github.byungtak.githubsearch.R
 
-fun FragmentActivity.replaceFragment(fragment: Fragment, tag: String = "", frameId: Int)
-= supportFragmentManager
-    .beginTransaction()
-    .setCustomAnimations(R.anim.pop_enter, R.anim.pop_exit)
-    .replace(frameId, fragment)
-    .commit()
+fun FragmentActivity.replaceFragment(fragment: Fragment, tag: String = "", frameId: Int) {
+    supportFragmentManager
+        .beginTransaction()
+        .setCustomAnimations(R.anim.pop_enter, R.anim.pop_exit)
+        .replace(frameId, fragment)
+        .commit()
+}
+
+fun AppCompatActivity.addFragment(@IdRes containerViewId: Int, fragment: Fragment, tag: String) {
+    supportFragmentManager.beginTransaction()
+        .add(containerViewId, fragment, tag)
+        .commitNow()
+}
+
+fun AppCompatActivity.showFragment(tag: String) {
+    val fragment = supportFragmentManager.findFragmentByTag(tag)
+
+    fragment?.let {
+        supportFragmentManager.beginTransaction()
+        .show(it)
+        .commitNow()
+    }
+}
+
+fun AppCompatActivity.hideFragment(tag: String) {
+    val fragment = supportFragmentManager.findFragmentByTag(tag)
+
+    fragment?.let {
+        supportFragmentManager.beginTransaction()
+        .hide(it)
+        .commitNow()
+    }
+}
--- app/src/main/java/com/github/byungtak/githubsearch/ui/MainActivity.kt
+++ app/src/main/java/com/github/byungtak/githubsearch/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.github.byungtak.githubsearch.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.github.byungtak.githubsearch.R
-import com.github.byungtak.githubsearch.extension.replaceFragment
+import com.github.byungtak.githubsearch.extension.addFragment
+import com.github.byungtak.githubsearch.extension.hideFragment
+import com.github.byungtak.githubsearch.extension.showFragment
 import com.github.byungtak.githubsearch.ui.favorite.FavoriteFragment
 import com.github.byungtak.githubsearch.ui.search.SearchFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,26 +17,40 @@ internal class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModel<MainViewModel>()
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                replaceFragment(SearchFragment.newInstance(), SearchFragment.TAG, R.id.container)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                replaceFragment(FavoriteFragment.newInstance(), FavoriteFragment.TAG, R.id.container)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        replaceFragment(SearchFragment.newInstance(), SearchFragment.TAG, R.id.container)
-
-        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        setupFragments()
+        setupListeners()
     }
+
+    private fun setupFragments() {
+        addFragment(R.id.container, SearchFragment.newInstance(), SearchFragment.TAG)
+        addFragment(R.id.container, FavoriteFragment.newInstance(), FavoriteFragment.TAG)
+
+        hideFragment(FavoriteFragment.TAG)
+    }
+
+    private fun setupListeners() {
+        bottom_navigation.setOnNavigationItemSelectedListener(object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                hideFragment(SearchFragment.TAG)
+                hideFragment(FavoriteFragment.TAG)
+
+                return when (item.itemId) {
+                    R.id.navigation_search -> {
+                        showFragment(SearchFragment.TAG)
+                        true
+                    }
+                    R.id.navigation_favorite -> {
+                        showFragment(FavoriteFragment.TAG)
+                        true
+                    }
+                    else -> false
+                }
+            }
+        })
+    }
+
 }

86cea199ef24f3f9003b38032409ed77afa561ee
Tak <tak8997@gmail.com>
2019-05-27 01:17:35 +0900

    searchViewModel test

---
+++ domain/src/main/java/com/github/byungtak/domain/common/TestTransformer.kt
@@ -0,0 +1,10 @@
+package com.github.byungtak.domain.common
+
+import io.reactivex.Observable
+import io.reactivex.ObservableSource
+
+class TestTransformer<T>: Transformer<T>() {
+    override fun apply(upstream: Observable<T>): ObservableSource<T> {
+        return upstream
+    }
+}
